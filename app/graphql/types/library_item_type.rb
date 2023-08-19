053beb6396f7e7cacab535162a2d28aa49ba3492
Pedro Augusto <pedrogusferva@gmail.com>
2023-08-19 14:52:42 -0300

    created LibraryItemType

--- app/graphql/types/library_item_type.rb
+++ app/graphql/types/library_item_type.rb
@@ -2,5 +2,9 @@
 
 module Types
   class LibraryItemType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :description, String, null: true
+    field :image_url, String, null: true
   end
 end
